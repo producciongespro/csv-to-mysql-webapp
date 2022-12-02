fe5dc4e46d9d9bdc572bb2f844b3f7aa29a13cf9
lchaconc <luis.chacon.campos@gmail.com>
2022-12-02 14:57:37 -0600

    FAse de envio de mesnaje desde el cliente -instalación de socket io -creación de objeto soket -llamado método emit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secreta"
+socketio = SocketIO(app)
 
 
 @app.route("/")
@@ -11,8 +13,14 @@ def home ():
     return render_template(  "index.html")
 
 
+@socketio.on('message')
+def handleMEssage(msg):
+    print ("Message", msg)
+
+
 
 
 
 if __name__ == "__main__":
-    app.run( debug=True )
+    socketio.run(app) 
+    #app.run( debug=True )
