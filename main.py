e0cc36d85383d81101f77d12d7eaf6ddbf4c7b16
lchaconc <luis.chacon.campos@gmail.com>
2022-12-02 15:41:52 -0600

    imp envio de mensajes desde servidor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secreta"
@@ -16,11 +17,18 @@ def home ():
 @socketio.on('message')
 def handleMEssage(msg):
     print ("Message", msg)
+    resend()
+
+def resend ():
+    send("hola desde el server")
+    time.sleep(2)
+    send ("Hola de nuevo")
+    time.sleep(2)
+    send ("Nos vemos...")
 
 
 
 
 
 if __name__ == "__main__":
-    socketio.run(app) 
-    #app.run( debug=True )
+    socketio.run(app)     
